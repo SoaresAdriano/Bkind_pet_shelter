f1f93f23bb7bd06a1c25cbb3cc9453e50483b376
Adriano Soares <adriano@debian-BULLSEYE-live-builder-AMD64>
2022-07-10 16:05:50 -0300

    chore: implements validator

--- src/services/petCadaster_service/domain/validator.go
+++ src/services/petCadaster_service/domain/validator.go
@@ -2,6 +2,7 @@ package domain
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-playground/validator/v10"
 )
@@ -17,4 +18,24 @@ type Validator struct {
 
 func NewValidator(ctx context.Context) (Validator, error) {
 	return Validator{}, ErrNotImplemented
+}
+
+func MustNewValidator(ctx context.Context) *Validator {
+	validator, err := NewValidator(ctx)
+	if err != nil {
+		panic(err)
+	}
+	return &validator
+}
+
+func (v Validator) Validate(obj interface{}) error {
+	err := v.validator.Struct(obj)
+	if err == nil {
+		return err
+	}
+
+	for _, e := range err.(validator.ValidationErrors) {
+		fmt.Printf("field validation error: %s \n tag: %s", e.Field(), e.Tag())
+	}
+	return err
 }
