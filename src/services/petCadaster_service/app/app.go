5c6511c0bd8936ae00834fd262bfe312292998cb
SoaresAdriano <dev.adriano.soares@gmail.com>
2022-07-23 00:28:29 -0300

    chore: implement app.go on application layer

--- src/services/petCadaster_service/app/app.go
+++ src/services/petCadaster_service/app/app.go
@@ -5,3 +5,24 @@ import "bkind_pet_shelter/src/services/petCadaster_service/domain"
 type AppParams struct {
 	PetsRepository domain.PetsRepository
 }
+
+type app struct {
+	petsRepository domain.PetsRepository
+}
+
+func NewApp(params AppParams) (domain.Application, error) {
+	if params.PetsRepository == nil {
+		return nil, domain.ErrMissingPetsRepositoryDependency
+	}
+	return app{
+		petsRepository: params.PetsRepository,
+	}, nil
+}
+
+func MustNewApp(params AppParams) domain.Application {
+	app, err := NewApp(params)
+	if err != nil {
+		panic(err)
+	}
+	return app
+}
